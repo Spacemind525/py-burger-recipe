25bfefcb7f31618c76537fc8761ac11e3668ac75
Alex <zigzaksasha@gmail.com>
2025-05-26 18:42:52 +0200

    We made delicious food!

--- app/main.py
+++ app/main.py
@@ -8,17 +8,17 @@ class Validator(ABC):
     def __get__(self, instance: "BurgerRecipe", owner: str) -> None:
         return getattr(instance, self.protected_name)
 
-    def __set__(self, instance, value) -> None:
+    def __set__(self, instance: str, value: str) -> None:
         self.validate(value)
         return setattr(instance, self.protected_name, value)
 
     @abstractmethod
-    def validate(self, value) -> None:
+    def validate(self, value: str) -> None:
         raise NotImplementedError("Subclasses must implement validate()")
 
 
 class Number(Validator):
-    def __init__(self, min_value, max_value) -> None:
+    def __init__(self, min_value: int, max_value: int) -> None:
         self.min_value = min_value
         self.max_value = max_value
 
@@ -32,10 +32,10 @@ class Number(Validator):
 
 
 class OneOf(Validator):
-    def __init__(self, options) -> None:
+    def __init__(self, options: tuple) -> None:
         self.options = options
 
-    def validate(self, value) -> None:
+    def validate(self, value: str) -> None:
         if value not in self.options:
             raise ValueError(f"Expected {value} to be one of {self.options}.")
 
@@ -46,7 +46,7 @@ class BurgerRecipe:
     tomatoes = Number(0, 3)
     cutlets = Number(1, 3)
     eggs = Number(0, 2)
-    sauce = OneOf(('ketchup', 'mayo', 'burger'))
+    sauce = OneOf(("ketchup", "mayo", "burger"))
 
     def __init__(self,
                  buns: int,
